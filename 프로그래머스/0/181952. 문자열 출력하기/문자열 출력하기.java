ac79c9d57f98837d89d1ce129ceafecdd38b18bf
Minseo Jang <120665565+mingdodev@users.noreply.github.com>
2024-03-20 00:10:36 +0900

    [level 0] Title: 문자열 출력하기, Time: 224.83 ms, Memory: 66.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
@@ -0,0 +1,18 @@
+// import java.util.Scanner;
+
+// public class Solution {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         String a = sc.next();
+//     }
+// }
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Scanner 클래스의 인스턴스 생성
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(str);
+    }
+}
