fe259dbc893e360a95db2e5b374d8378fad51cb9
Minseo Jang <120665565+mingdodev@users.noreply.github.com>
2025-09-09 15:20:23 +0900

    [level 2] Title: 가장 큰 수, Time: 257.51 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 class Solution {
     public String solution(int[] numbers) {
-        String[] stringNumbers = Arrays.stream(numbers).mapToObj(Integer::toString).toArray(String[]::new);
+        String[] stringNumbers = Arrays.stream(numbers).mapToObj(String::valueOf).toArray(String[]::new);
         Arrays.sort(stringNumbers, (a, b) -> { return (b + a).compareTo(a + b); });
         StringBuilder sb = new StringBuilder("");
         for (String s : stringNumbers) {
