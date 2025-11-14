b97d2b60f6dcc79658ed5cdbc12e0f2021155b48
Minseo Jang <120665565+mingdodev@users.noreply.github.com>
2025-11-14 22:39:36 +0900

    [level 2] Title: 타겟 넘버, Time: 6.55 ms, Memory: 85.8 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,16 +1,16 @@
 class Solution {
     public int solution(int[] numbers, int target) {
         
-        return dfs(-numbers[0], numbers, target, 0, 0)
-            + dfs(numbers[0], numbers, target, 0, 0);
+        return dfs(-numbers[0], numbers, target, 0)
+            + dfs(numbers[0], numbers, target, 0);
     }
-    private int dfs(int sum, int[] numbers, int target, int i, int answer) {
+    private int dfs(int sum, int[] numbers, int target, int i) {
         if (i == numbers.length - 1) {
             if (sum == target) return 1;
             return 0;
         }
         
-        return dfs(sum - numbers[i + 1], numbers, target, i + 1, answer)
-            + dfs(sum + numbers[i + 1], numbers, target, i + 1, answer);
+        return dfs(sum - numbers[i + 1], numbers, target, i + 1)
+            + dfs(sum + numbers[i + 1], numbers, target, i + 1);
     }
 }
