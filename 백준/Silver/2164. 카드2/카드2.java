9c4a285b661db563dcc01c277d26380bf323b0e5
Minseo Jang <120665565+mingdodev@users.noreply.github.com>
2026-03-05 21:57:39 +0900

    [Silver IV] Title: 카드2, Time: 168 ms, Memory: 28852 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        Deque<Integer> q = new ArrayDeque<>();
+
+        for (int i = 1; i <= n; i++) {
+            q.addLast(i);
+        }
+
+        while (q.size() != 1) {
+            q.removeFirst();
+            q.addLast(q.removeFirst());
+        }
+
+        System.out.println(q.poll());
+    }
+}
