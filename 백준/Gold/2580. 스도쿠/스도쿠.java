a25710870e4215ec76232e37c9de5b981eb0df63
Minseo Jang <120665565+mingdodev@users.noreply.github.com>
2026-04-13 17:55:53 +0900

    [Gold IV] Title: 스도쿠, Time: 248 ms, Memory: 16864 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.java
+++ 백준/Gold/2580. 스도쿠/스도쿠.java
@@ -6,7 +6,7 @@ public class Main {
     static boolean[][] raw = new boolean[9][10];
     static boolean[][] column = new boolean[9][10];
     static boolean[][] miniBoard = new boolean[9][10];
-    static int blank = 0;
+    static List<int[]> blanks = new ArrayList<>();;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,7 +17,7 @@ public class Main {
                 int temp = Integer.parseInt(st.nextToken());
                 board[i][j] = temp;
                 if (temp == 0)
-                    blank++;
+                    blanks.add(new int[] {i, j});
                 else {
                     raw[i][temp] = true;
                     column[j][temp] = true;
@@ -26,26 +26,16 @@ public class Main {
             }
         }
 
-        // 0인 부분에 가능한 경우의 수를 채워나가며 판단한다.
-        // 가능한지 판단은 다음 두 가지 기준
-        // 1. board[i][x]에 없는 수 (0인가?)
-        // 2. board[y][i]에 없는 수 (0인가?)
-        // 3. 부분 정사각형에 없는 수 (false인가?)
-        // 9*3번의 비교 연산, 최대 27*81이므로 ㄱㅊ
-
-        dfs(0, 0);
+        dfs(0);
 
     }
-    private static void dfs(int x, int y) {
-        if (blank == 0) { // 정답을 찾았으면 탐색 종료
+    private static boolean dfs(int idx) {
+        if (idx == blanks.size()) { // 정답을 찾았으면 탐색 종료
             printBoard();
-            return;
+            return true;
         }
-
-        if (board[y][x] != 0) { // 빈칸이 아니면 다음 칸으로 이동
-            move(x, y);
-            return;
-        }
+        int x = blanks.get(idx)[1];
+        int y = blanks.get(idx)[0];
 
         // 빈칸이라면 알맞은 숫자 채우기
         for (int t = 1; t <= 9; t++) {
@@ -54,24 +44,16 @@ public class Main {
             raw[y][t] = true;
             column[x][t] = true;
             miniBoard[(y / 3) * 3 + x / 3][t] = true;
-            blank--;
-            move(x, y);
-            if (blank == 0) break;
-            board[y][x] = 0;
-            raw[y][t] = false;
-            column[x][t] = false;
-            miniBoard[(y / 3) * 3 + x / 3][t] = false;
-            blank++;
+            if (dfs(idx + 1)) return true;
+            else {
+                board[y][x] = 0;
+                raw[y][t] = false;
+                column[x][t] = false;
+                miniBoard[(y / 3) * 3 + x / 3][t] = false;
+            }
         }
+        return false;
     }
-
-    private static void move(int x, int y) {
-        if (x == 8)
-            dfs(0, y + 1);
-        else
-            dfs(x + 1, y);
-    }
-
     private static void printBoard() {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < 9; i++) {
