addf5c31dc1cb31815d39292de6fda860bb5df08
Minseo Jang <120665565+mingdodev@users.noreply.github.com>
2026-04-15 12:12:28 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 912 ms, Memory: 92236 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+
+        List<List<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<>());
+        }
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            graph.get(u).add(v);
+            graph.get(v).add(u);
+        }
+        for (int i = 1; i <= n; i++) {
+            Collections.sort(graph.get(i));
+        }
+
+        int[] visited = new int[n + 1];
+        Deque<Integer> queue = new ArrayDeque<>();
+
+        int index = 1;
+        queue.addLast(r);
+        visited[r] = index++;
+
+        while(!queue.isEmpty()) {
+            int temp = queue.pollFirst();
+            for (int i : graph.get(temp)) {
+                if (visited[i] == 0) {
+                    queue.addLast(i);
+                    visited[i] = index++;
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            sb.append(visited[i]).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
