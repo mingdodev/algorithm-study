eb65d511285aa7c94cbbd0a793cbff526101d81b
Minseo Jang <120665565+mingdodev@users.noreply.github.com>
2026-04-20 01:39:00 +0900

    [Gold V] Title: 토마토, Time: 640 ms, Memory: 172128 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -2,76 +2,71 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static class Tomato {
+    private static class Tomato {
         int x;
         int y;
-        int days;
 
-        public Tomato(int x, int y, int days) {
+        public Tomato(int x, int y) {
             this.x = x;
             this.y = y;
-            this.days = days;
         }
     }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int m = Integer.parseInt(st.nextToken());
         int n = Integer.parseInt(st.nextToken());
-        int[][] box = new int[n][m];
 
-        Deque<Tomato> ripen = new ArrayDeque<>();
+        int[][] graph = new int[n][m];
+        List<Tomato> start = new ArrayList<>();
+        // bfs를 하는데, 시작점이 여러 개.
+        // 1일차 - 익은 토마토 입력 받은 거 -> 토마토의 개수가 m * n 개인가?
+        // 2일차 - bfs 1단계
+        // 스택이 비었을 때 -> 토마토 개수가 m * n 개보다 작으면 -1
 
-        // 처음부터 다 넣지말고, 익은 것만 넣기
-        // BFS는 어떻게 구현하지? 큐?
-        // 익은 거 다 넣어두고 사방으로 익지 않은 토마토 체크하기 -> 근데 이러면 일자 계산은 어떻게 하지
-
-        int k;
-        int empty = 0;
+        // 좌표 표현 필요, 시작점만 따로 저장하기
+        int all = m * n;
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < m; j++) {
-                k = Integer.parseInt(st.nextToken());
-                box[i][j] = k;
-                if (k == 1) {
-                    ripen.offerLast(new Tomato(i, j, 0));
-                }
-                if (k == -1) {
-                    empty++;
-                }
+                int box = Integer.parseInt(st.nextToken());
+                graph[i][j] = box;
+                if (box == 1) start.add(new Tomato(j, i));
+                if (box == -1) all--;
             }
         }
 
-        // 사방 탐색을 위한 방향 배열
-        int[] dx = {-1, 1, 0, 0};
-        int[] dy = {0, 0, -1, 1};
-        int result = 0;
-        int ripenTomatoes = 0;
+        int count = start.size();
+        if (count == all) {
+            System.out.println(0);
+            return;
+        }
+        Deque<Tomato> queue = new ArrayDeque<>();
+        for (Tomato t : start) {
+            queue.addLast(t);
+        }
+        while (!queue.isEmpty()) {
+            Tomato temp = queue.pollFirst();
 
-        while (!ripen.isEmpty()) {
-            Tomato t = ripen.pollFirst();
-            result = Math.max(result, t.days); // 리프 노드를 검사할 때 최대 값 저장 가능
-            ripenTomatoes++;
-
-            for (int d = 0; d < 4; d++) {
-                int nx = t.x + dx[d];
-                int ny = t.y + dy[d];
-
-                // 좌표 범위를 벗어나거나, 익지 않은 토마토가 아닐 경우는 스킵
-                if (nx < 0 || ny < 0 || ny >= m || nx >= n) continue;
-                if (box[nx][ny] != 0) continue;
-
-                box[nx][ny] = 1;
-                ripen.offerLast(new Tomato(nx, ny, t.days + 1));
+            int[] dx = { -1, 1, 0, 0 };
+            int[] dy = { 0, 0, -1, 1 };
+            for (int i = 0; i < 4; i++) {
+                int nx = temp.x + dx[i];
+                int ny = temp.y + dy[i];
+                if (nx < 0 || nx >= m || ny < 0 || ny >= n) continue;
+                if (graph[ny][nx] == 0) {
+                    queue.addLast(new Tomato(nx, ny));
+                    graph[ny][nx] = graph[temp.y][temp.x] + 1;
+                    count++;
+                    if (count == all) {
+                        System.out.println(graph[ny][nx] - 1);
+                        return;
+                    }
+                }
             }
         }
-
-        if (n * m - empty > ripenTomatoes) {
+        if (count < all) {
             System.out.println(-1);
-        } else {
-            // 처음부터 모두 익었을 경우에 0 출력 가능
-            System.out.println(result);
         }
     }
 }
